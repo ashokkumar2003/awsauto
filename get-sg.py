84c61fc24d65e383cef7c7237cdbd8a8deb2b053
Ashok Kumar <ashokkumar@freeunixsolutions.com>
2018-02-21 11:37:19 +0530

    added

--- get-sg.py
+++ get-sg.py
@@ -5,7 +5,7 @@ import time
 
 autoScalingGroups = [['pb-lg', 'HostSG1']]
 for asg in autoScalingGroups:
-    conn = boto.ec2.connect_to_region("us-east-1",aws_access_key_id='AKIAJX5GCTO72BO55PLQ',aws_secret_access_key='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')
+    conn = boto.ec2.connect_to_region("us-east-1",aws_access_key_id='AKIAJNGNSIMWUK4M5TIA',aws_secret_access_key='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')
     reservations = conn.get_all_instances(filters={"tag:aws:autoscaling:groupName" : asg[0]})
     list_instances = ""
     for reservation in reservations:
